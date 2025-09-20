464c27ffb5c9f245f7eee69fbf995930cf3d73b3
Ohidul Islam <251-15-917@diu.edu.bd>
2025-09-20 23:12:13 +0600

    Add icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:downloader/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
   }
 }
