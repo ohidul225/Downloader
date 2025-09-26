125cd986f6194097503e8fce5b0aba4f2e73ad60
Ohidul Islam <251-15-917@diu.edu.bd>
2025-09-26 23:04:26 +0600

    Add navigation button

--- lib/pages/video_download_screen.dart
+++ lib/pages/video_download_screen.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class VideoDownloadScreen extends StatelessWidget {
@@ -5,6 +6,27 @@ class VideoDownloadScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 181, 208, 231),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 181, 208, 231),
+        color: const Color.fromARGB(255, 147, 167, 185),
+        animationDuration: Duration(milliseconds: 300),
+        items: [
+          Icon(
+            Icons.video_library_outlined,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.music_note,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.settings,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
   }
 }
